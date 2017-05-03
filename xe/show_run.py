4a26a4398aabed494a64708f93a8323fe538dd1c
npdesi <cosine@freesoft.org>
2017-05-03 01:43:38 +0000

    The show_run.py script is a basic NETCONF script for the XE OS.

---
+++ xe/show_run.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+import code
+from lxml import etree
+from ncclient import manager
+
+if __name__ == "__main__":
+
+    with manager.connect(host='52.55.197.114', port=830, username='brent', password='baccala',
+                         hostkey_verify=False, device_params={'name': 'csr'},
+                         allow_agent=False, look_for_keys=False) as device:
+
+
+        nc_filter = """
+                <config>
+                </config>
+        """
+
+        for cap in device.server_capabilities:
+            print cap
+
+        nc_reply = device.get_config('running')
+        # print nc_reply
+        print etree.tostring(nc_reply.data_ele, pretty_print=True)
+
+        code.interact(None, None, locals())
