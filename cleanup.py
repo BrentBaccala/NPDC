8ae3e30182c0aa13d8883bfcb4d1cc65e5db670e
Brent Baccala <baccala@freesoft.org>
2017-08-30 15:48:45 -0400

    cleanup.py: use dictionary comprehension

--- cleanup.py
+++ cleanup.py
@@ -3,9 +3,7 @@ preserve_types = [type(''), type(u''), type(0), type(True)]
 
 def cleanup(data):
   if (type(data) == type({})):
-    for k in data.keys():
-      data[k] = cleanup(data[k])
-    return data
+    return {k: cleanup(data[k]) for k in data.keys()}
   elif (type(data) == type([])):
     return [cleanup(i) for i in data]
   elif type(data) in preserve_types:
