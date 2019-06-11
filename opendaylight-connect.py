75e8b210a01aee2134e5c672a41f8b575e8a99a9
Brent Baccala <cosine@freesoft.org>
2019-06-11 15:04:20 -0400

    update opendaylight connector

--- opendaylight-connect.py
+++ opendaylight-connect.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python
+#
+# Connect csr3 (which should be listening for NETCONF calls) to OpenDaylight on blade8
 
 import requests
 from requests.auth import HTTPBasicAuth
@@ -14,44 +16,6 @@ headers = {'Content-Type': 'application/xml',
            'Accept': 'application/xml'}
 
 # Copied from https://docs.opendaylight.org/en/stable-oxygen/user-guide/netconf-user-guide.html
-
-url1 = 'http://blade8:8181/restconf/config/network-topology:network-topology/topology/topology-netconf/node/controller-config/yang-ext:mount/config:modules'
-
-payload1 = '''
-<module xmlns="urn:opendaylight:params:xml:ns:yang:controller:config">
-  <type xmlns:prefix="urn:opendaylight:params:xml:ns:yang:controller:md:sal:connector:netconf">prefix:sal-netconf-connector</type>
-  <name>new-netconf-device</name>
-  <address xmlns="urn:opendaylight:params:xml:ns:yang:controller:md:sal:connector:netconf">127.0.0.1</address>
-  <port xmlns="urn:opendaylight:params:xml:ns:yang:controller:md:sal:connector:netconf">830</port>
-  <username xmlns="urn:opendaylight:params:xml:ns:yang:controller:md:sal:connector:netconf">admin</username>
-  <password xmlns="urn:opendaylight:params:xml:ns:yang:controller:md:sal:connector:netconf">admin</password>
-  <tcp-only xmlns="urn:opendaylight:params:xml:ns:yang:controller:md:sal:connector:netconf">false</tcp-only>
-  <event-executor xmlns="urn:opendaylight:params:xml:ns:yang:controller:md:sal:connector:netconf">
-    <type xmlns:prefix="urn:opendaylight:params:xml:ns:yang:controller:netty">prefix:netty-event-executor</type>
-    <name>global-event-executor</name>
-  </event-executor>
-  <binding-registry xmlns="urn:opendaylight:params:xml:ns:yang:controller:md:sal:connector:netconf">
-    <type xmlns:prefix="urn:opendaylight:params:xml:ns:yang:controller:md:sal:binding">prefix:binding-broker-osgi-registry</type>
-    <name>binding-osgi-broker</name>
-  </binding-registry>
-  <dom-registry xmlns="urn:opendaylight:params:xml:ns:yang:controller:md:sal:connector:netconf">
-    <type xmlns:prefix="urn:opendaylight:params:xml:ns:yang:controller:md:sal:dom">prefix:dom-broker-osgi-registry</type>
-    <name>dom-broker</name>
-  </dom-registry>
-  <client-dispatcher xmlns="urn:opendaylight:params:xml:ns:yang:controller:md:sal:connector:netconf">
-    <type xmlns:prefix="urn:opendaylight:params:xml:ns:yang:controller:config:netconf">prefix:netconf-client-dispatcher</type>
-    <name>global-netconf-dispatcher</name>
-  </client-dispatcher>
-  <processing-executor xmlns="urn:opendaylight:params:xml:ns:yang:controller:md:sal:connector:netconf">
-    <type xmlns:prefix="urn:opendaylight:params:xml:ns:yang:controller:threadpool">prefix:threadpool</type>
-    <name>global-netconf-processing-executor</name>
-  </processing-executor>
-  <keepalive-executor xmlns="urn:opendaylight:params:xml:ns:yang:controller:md:sal:connector:netconf">
-    <type xmlns:prefix="urn:opendaylight:params:xml:ns:yang:controller:threadpool">prefix:scheduled-threadpool</type>
-    <name>global-netconf-ssh-scheduled-executor</name>
-  </keepalive-executor>
-</module>
-'''
 
 url2 = "http://blade8:8181/restconf/config/network-topology:network-topology/topology/topology-netconf/node/csr3"
 
