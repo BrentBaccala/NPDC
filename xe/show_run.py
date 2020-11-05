a813f14d26afece2661a289c443986185820ec36
Brent Baccala <cosine@freesoft.org>
2020-11-04 19:59:58 -0500

    xe/show_run.py - convert to python 3

--- xe/show_run.py
+++ xe/show_run.py
@@ -1,12 +1,15 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
+import sys
 import code
 from lxml import etree
 from ncclient import manager
 
+
+
 if __name__ == "__main__":
 
-    with manager.connect(host='52.55.197.114', port=830, username='brent', password='baccala',
+    with manager.connect(host=sys.argv[1], port=830, username='admin', password='admin',
                          hostkey_verify=False, device_params={'name': 'csr'},
                          allow_agent=False, look_for_keys=False) as device:
 
@@ -17,10 +20,10 @@ if __name__ == "__main__":
         """
 
         for cap in device.server_capabilities:
-            print cap
+            print(cap)
 
         nc_reply = device.get_config('running')
         # print nc_reply
-        print etree.tostring(nc_reply.data_ele, pretty_print=True)
+        print(etree.tostring(nc_reply.data_ele, pretty_print=True))
 
         code.interact(None, None, locals())
